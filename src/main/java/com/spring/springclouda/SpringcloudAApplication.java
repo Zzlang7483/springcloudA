fd32c359a7ee9c18d02be66bc0896261bea4fbee
zhangsan <252012798@qq.com>
2023-03-09 09:03:00 +0800

    Initial commit

---
+++ src/main/java/com/spring/springclouda/SpringcloudAApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springclouda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springclouda/SpringcloudAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springclouda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
